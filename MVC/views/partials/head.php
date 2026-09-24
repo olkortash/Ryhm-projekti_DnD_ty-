d29581ace016b94a707632e1b99735b629f9566a
Jyri Kuikka <jyri.kuikka@gmail.com>
2026-09-24 13:50:39 +0300

    fixed (maybe)

--- MVC/views/partials/head.php
+++ MVC/views/partials/head.php
@@ -61,13 +61,3 @@ $pageTitle = $pageTitle ?? 'Masters';
     </div>
 
 </header>
-<<<<<<< HEAD
-=======
-
-<?php if (!empty($_SESSION['flash'])): ?>
-    <?php $flash = $_SESSION['flash']; unset($_SESSION['flash']); ?>
-    <div class="flash-message flash-<?= e($flash['type'] ?? 'info'); ?>" role="status">
-        <?= e($flash['message'] ?? ''); ?>
-    </div>
-<?php endif; ?>
->>>>>>> ed8022b655d37e9b096be1318706178b5cd67f29
